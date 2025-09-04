50656560e65caccc88c07a4afcfec5963dd72f2b
Anderson Cataldo <andersonvcataldo@gmail.com>
2025-09-04 16:36:37 -0300

    Update TurmaView.java

--- src/TurmaView.java
+++ src/TurmaView.java
@@ -1,9 +1,15 @@
-public class TurmaView{
-    public void MostrarDados(){
-        System.out.println("total de alunos: " +Turma.getTotalAlunos());
+public class TurmaView {
+    public void MostrarDados(Turma turma) {
+        System.out.println("Total de alunos: "+turma.getTotalAlunos());
+        System.out.println("Média da turma: " + turma.calcularMedia());
     }
-
-    public void ImprimeDados(){
-        System.out.println("imprime dados");
+    public void ImprimeDados(Turma turma) {
+        System.out.println("=== Dados da Turma ===");
+        for (Aluno aluno : turma.getAlunos()) {
+            System.out.println("Nome: " + aluno.getNome());
+            System.out.println("Matrícula: " + aluno.getMatricula());
+            System.out.println("Nota: " + aluno.getNota());
+            System.out.println("----------------------");
+        }
     }
 }
